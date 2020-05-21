16d897b0e258a191d0a314fd1cf3c0003d7c0c01
Kisha Codes <Kishacodes@gmail.com>
2020-05-20 21:54:22 -0400

    Part 1 completed

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'fav-photos',
-  templateUrl: './fav-photos.component.html',
-  styleUrls: ['./fav-photos.component.css']
+  selector: "fav-photos",
+  templateUrl: "./fav-photos.component.html",
+  styleUrls: ["./fav-photos.component.css"],
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = "Pictures I love to see";
+  image1 =
+    "https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png";
+  image2 = "http://wallpapercave.com/wp/E6NlPya.jpg";
+  image3 = "http://getwallpapers.com/wallpaper/full/c/0/8/111746.jpg";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
