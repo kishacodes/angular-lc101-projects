795510483ef41a7426455cb95abad593b3c066e7
Kisha Codes <Kishacodes@gmail.com>
2020-05-20 22:21:53 -0400

    2. Working on Part 2. Component not showing

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: "movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8', 'The Color Purple', 'Coming to Ameria'];
+  movies = [
+    "The Manchurian Candidate",
+    "Oceans 8",
+    "The Color Purple",
+    "Coming to America",
+  ];
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
-
+  ngOnInit() {}
 }
