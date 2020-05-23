69906f56b6bf1139df2fafead98a420f12e25be2
Kisha Codes <Kishacodes@gmail.com>
2020-05-23 16:05:56 -0400

    3. Finished all parts of ngIf practice

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'chores',
-   templateUrl: './chores.component.html',
-   styleUrls: ['./chores.component.css']
+  selector: "chores",
+  templateUrl: "./chores.component.html",
+  styleUrls: ["./chores.component.css"],
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+  chores = ["Buy groceries", "Cook dinner"];
+  finishedChores = [
+    "Complete LaunchCode work",
+    "Empty dishwasher",
+    "Laundry",
+    "Dust shelves",
+    "Cut grass",
+  ];
 
-   trophyImage = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
+  treats = [
+    "The chores list must be empty.",
+    "The Done list must have at least 4 items.",
+  ];
 
-   constructor() { }
+  trophyImage =
+    "https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png";
 
-   ngOnInit() {
-   }
+  constructor() {}
 
+  ngOnInit() {}
 }
